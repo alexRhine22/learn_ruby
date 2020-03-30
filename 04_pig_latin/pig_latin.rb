23f0c453fd57170aa0db36eaa7a9035f89cfda53
alexRhine22 <alextherhine@gmail.com>
2020-03-30 16:32:07 -0700

    started working on pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,6 +1,10 @@
 #write your code here
-def translate(word)
+def translate(str)
     vowels_arr = ['a', 'e', 'i', 'o', 'u']
+    answer = ""
+    if (vowels_arr.include?(str[0]))
+        answer = str + "ay"
+    end
 
-    
+    return answer
 end
